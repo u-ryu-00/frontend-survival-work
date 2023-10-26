aa08631611081bd9c89365879a0bf9a313246d7b
u-ryu-00 <lwh76104@gmail.com>
2023-10-26 22:41:15 +0900

    8주차-4.styled-components

--- lecture-08/react-demo-app/src/Greeting.tsx
+++ lecture-08/react-demo-app/src/Greeting.tsx
@@ -1,17 +1,37 @@
-const darkMode = false;
+import styled from 'styled-components';
 
-function primaryColor() {
-  return darkMode ? '#F00' : '#00F';
+const Paragraph = styled.p`
+  color: #00F;
+
+  strong {
+    font-size: 2em;
+    color: #F00;
+  }
+`;
+
+const BigParagraph = styled(Paragraph)`
+  font-size: 2em;
+
+  strong {
+    font-size: 1.5em;
+  }
+`;
+
+function HelloWorld({ className }: React.HTMLAttributes<HTMLElement>) {
+  return (
+    <BigParagraph className={className}>
+      Hello, world
+      <strong>!</strong>
+    </BigParagraph>
+  );
 }
+
+const SmallHelloWorld = styled(HelloWorld)`
+  font-size: .1em;
+`;
 
 export default function Greeting() {
   return (
-    <p
-      style={{
-        color: primaryColor(),
-      }}
-    >
-      Hello, world!
-    </p>
+    <SmallHelloWorld />
   );
 }
